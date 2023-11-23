b87572832f731124abc5c37b11c784a5c25ada7e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-23 16:41:40 +0200

    Add or replace in the class BaseModel:
        class attribute id
            represents a column containing a unique string (60 characters)
            can’t be null
            primary key
        class attribute created_at
            represents a column containing a datetime
            can’t be null
            default value is the current datetime (use datetime.utcnow())
        class attribute updated_at
            represents a column containing a datetime
            can’t be null
            default value is the current datetime (use datetime.utcnow())
    
    Move the models.storage.new(self) from def __init__(self, *args, **kwargs): to def save(self): and call it just before models.storage.save()

--- models/base_model.py
+++ models/base_model.py
@@ -2,20 +2,26 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime, P
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if not kwargs:
-            from models import storage
-            storage.new(self)
-        else:
+        if kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -32,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):

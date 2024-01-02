617767a5805a754e82d6540e411d836ef8999db8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-02 16:01:23 +0200

    add do_deploy method

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,29 @@ import datetime
 import os
 
 
+def do_pack():
+    '''
+    archive from the contents of the web_static
+    '''
+    try:
+        if not os.path.exists('versions'):
+            os.makedirs('versions')
+
+        current_time = datetime.datetime.now()
+        file = "versions/web_static_{}{}{}{}{}{}.tgz" \
+            .format(current_time.year,
+                    current_time.month,
+                    current_time.day,
+                    current_time.hour,
+                    current_time.minute,
+                    current_time.second)
+        print(f'Packing web_static to {file}')
+        local(f"tar -cvzf {file} web_static")
+        return file
+    except Exception as e:
+        return None
+
+
 def do_deploy(archive_path):
     """distributes an archive to your web servers
 

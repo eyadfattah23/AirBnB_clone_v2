77c6e013a8035886178438ed975e6676acdf60e0
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-14 18:35:41 +0200

    use sudo for owner changing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -36,7 +36,7 @@ sudo echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group. This should be recursive; everything inside should be created/owned by this user/group.
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static

3831744eb79e18b0d269f368a43e8dd1ec77af35
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-28 06:52:13 +0200

    update 1- use completely different server web01 2- use sudo for everything

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,43 +32,44 @@ def do_deploy(archive_path):
 
     """
     try:
+        env.hosts = ['52.3.251.8', '100.25.162.125']
         env.user = 'ubuntu'
-        env.hosts = ['100.25.22.31', '100.25.162.125']
-
         # Return False if the file at the path archive_path doesn’t exist
         if not os.path.exists(archive_path):
             return False
 
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
+        for host in env.hosts:
+            env.host_string = host
+            # Upload the archive to the /tmp/ directory of the web server
+            put(archive_path, '/tmp/')
 
-        # mkdir if non-existent
-        folder_name = archive_path.split("/")[-1][:-4]
-        archive_name = archive_path.split("/")[-1]
-        run("mkdir -p /data/web_static/releases/{}".format(folder_name))
+            # mkdir if non-existent
+            folder_name = archive_path.split("/")[-1][:-4]
+            archive_name = archive_path.split("/")[-1]
+            run("sudo mkdir -p /data/web_static/releases/{}".format(folder_name))
 
-        # Uncompress the archive
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_name,
-                    folder_name))
+            # Uncompress the archive
+            run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+                .format(archive_name,
+                        folder_name))
 
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_name))
+            # Delete the archive from the web server
+            run('rm /tmp/{}'.format(archive_name))
 
-        # Delete the symbolic link /data/web_static/current from the web server
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(folder_name, folder_name))
+            # Delete the symbolic link /data/web_static/current from the web server
+            run('sudo mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/'.format(folder_name, folder_name))
 
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
+            run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
 
-        run('rm -rf /data/web_static/current')
+            run('sudo rm -rf /data/web_static/current')
 
-        # Create a new the symbolic link /data/web_static/current on web server,
-        # linked to the new version of your code
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(folder_name))
+            # Create a new the symbolic link /data/web_static/current on web server,
+            # linked to the new version of your code
+            run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'
+                .format(folder_name))
 
-        print('New version deployed!')
+            print('New version deployed!')
         return True
     except Exception as e:
         return False

ce2feec07a3e188e38cfd8321f42b2a43e3668fe
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 03:00:41 +0200

    add route /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+script that starts a Flask web application
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+'''
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display “HBNB”"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """display “C ” followed by the value of the text variable """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

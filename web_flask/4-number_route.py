1186241afceed72087341baa9e4e815c8a9e5413
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 03:09:21 +0200

    add route /number/<n>: display “n is a number” only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+script that starts a Flask web application
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+'''
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display “HBNB”"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """display “C ” followed by the value of the text variable """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """display “Python ” followed by the value of the text variable """
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """display “n is a number” only if n is an integer"""
+    return f"{escape(n)} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

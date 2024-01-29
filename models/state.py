1eb97dbc919ac00e7b59da2a589805058bfc7418
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 04:06:02 +0200

    Update State: (models/state.py)
        State inherits from BaseModel and Base (respect the order)
        Add or replace in the class State:
            class attribute __tablename__
                represents the table name, states
            class attribute name
                represents a column containing a string (128 characters)
                can’t be null
            for DBStorage: class attribute cities must represent a relationship with the class City. If the State object is deleted, all linked City objects must be automatically deleted. Also, the reference from a City object to his State should be named state
            for FileStorage: getter attribute cities that returns the list of City instances with state_id equals to the current State.id => It will be the FileStorage relationship between State and City

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
+from models import HBNB_TYPE_STORAGE
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+
+    cities = relationship("City", cascade='all, delete', backref='state')
+
+    if HBNB_TYPE_STORAGE != 'db':
+        @property
+        def cities(self):
+            '''getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id'''
+            cits_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cits_list.append(city)
+            return cits_list

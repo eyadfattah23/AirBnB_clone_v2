3a5fe6cf18dc33b19154c83c2389ebcc80b98cd4
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 08:09:54 +0200

    add missing imports

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):

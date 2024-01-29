764226390bd9870415e3f1fdc3e974eaf29d5abd
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 04:06:25 +0200

    New engine DBStorage: (models/engine/db_storage.py)
        Private class attributes:
            __engine: set to None
            __session: set to None
        Public instance methods:
            __init__(self):
                create the engine (self.__engine)
                the engine must be linked to the MySQL database and user created before (hbnb_dev and hbnb_dev_db):
                    dialect: mysql
                    driver: mysqldb
                all of the following values must be retrieved via environment variables:
                    MySQL user: HBNB_MYSQL_USER
                    MySQL password: HBNB_MYSQL_PWD
                    MySQL host: HBNB_MYSQL_HOST (here = localhost)
                    MySQL database: HBNB_MYSQL_DB
                don’t forget the option pool_pre_ping=True when you call create_engine
                drop all tables if the environment variable HBNB_ENV is equal to test
            all(self, cls=None):
                query on the current database session (self.__session) all objects depending of the class name (argument cls)
                if cls=None, query all types of objects (User, State, City, Amenity, Place and Review)
                this method must return a dictionary: (like FileStorage)
                    key = <class-name>.<object-id>
                    value = object
            new(self, obj): add the object to the current database session (self.__session)
            save(self): commit all changes of the current database session (self.__session)
            delete(self, obj=None): delete from the current database session obj if not None
            reload(self):
                create all tables in the database (feature of SQLAlchemy) (WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine))
                create the current database session (self.__session) from the engine (self.__engine) by using a sessionmaker - the option expire_on_commit must be set to False ; and scoped_session - to make sure your Session is thread-safe

---
+++ models/engine/db_storage.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""This module defines a class to manage DB storage for hbnb clone"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from models.base_model import Base
+
+
+class DBStorage:
+    '''defines a class to manage DB storage for hbnb clone'''
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        '''initializes a new instance of DBStorage
+        create the engine (self.__engine)
+        linked to the MySQL database and user created before'''
+
+        MySQL_user = getenv('HBNB_MYSQL_USER')
+        MySQL_password = getenv('HBNB_MYSQL_PWD')
+        MySQL_host = getenv('HBNB_MYSQL_HOST')
+        MySQL_database = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(
+                                          MySQL_user,
+                                          MySQL_password,
+                                          MySQL_host,
+                                          MySQL_database), pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        '''query on the current database session (self.__session) all objects
+        depending of the class name (argument cls)'''
+        objects = {}
+        if cls is None:
+            from models.user import User
+            from models.state import State
+            from models.review import Review
+            from models.place import Place
+            from models.city import City
+            from models.amenity import Amenity
+            classes = {'user': User, 'place': Place,
+                       'amenity': Amenity, 'state': State,
+                       'review': Review, 'city': City}
+
+            for c in classes.values():
+                records = self.__session.query(c).all()
+                for object in records:
+                    objects.update(
+                        {object.__class__.__name__ + '.' + object.id: object})
+
+        else:
+            records = self.__session.query(cls).all()
+
+            for object in records:
+                objects.update(
+                    {object.__class__.__name__ + '.' + object.id: object})
+        return objects
+
+    def new(self, obj):
+        '''add the object to the current database session'''
+        self.__session.add(obj)
+
+    def save(self):
+        '''commit all changes of the current database session'''
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        '''delete from the current database session obj if not None'''
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        '''create all tables in the database
+        create the current database session
+        from the engine by using a sessionmaker
+        '''
+        from models.user import User
+        from models.state import State
+        from models.review import Review
+        from models.place import Place
+        from models.city import City
+        from models.amenity import Amenity
+        Base.metadata.create_all(self.__engine)
+
+        self.__session = scoped_session(sessionmaker(
+            self.__engine, expire_on_commit=False))

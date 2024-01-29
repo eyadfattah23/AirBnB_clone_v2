02a603f390e938c596ff6973c292dfc1d9a8fd38
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-29 07:14:55 +0200

    add the connection

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Active: 1706494701579@@127.0.0.1@3306
 -- script that prepares a MySQL server for the project:
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';

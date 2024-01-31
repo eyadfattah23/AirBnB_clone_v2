a2ce4b43ca6935019f6e9b0981860965a858dd2f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-31 07:56:55 +0200

    try someone else's code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,61 +1,45 @@
 #!/usr/bin/python3
 """
-- script that starts a Flask web application
+A script to To load all cities of a State,
+If storage engine is DBStorage, use cities relationship
+Otherwise, use the public getter method cities.
+After each request remove the current SQLAlchemy Session,
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
 
-- web application listening on 0.0.0.0, port 5000
-
-- use storage for fetching data from the storage engine
-
-- After each request you must remove the current SQLAlchemy Session:
-    Declare a method to handle @app.teardown_appcontext
-    Call in this method storage.close()
-
-- Routes:
-    /states_list: display a HTML page
-    /cities_by_states: display a HTML page
 """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-from markupsafe import escape
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """display “Hello HBNB!”"""
+def home():
+    """A function that serves as the home route of the application.
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
     return "Hello HBNB!"
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays cities per state"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """After each request remove the current SQLAlchemy Session"""
+def teardown_context(ctx):
+    """Displays cities per state"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """display a HTML page
-
-    Returns:
-        html file: the list of all State objects present in DBStorage
-    """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def list_cities_by_state():
-    """display a HTML page
-
-    Returns:
-        html file: the list of all State objects present in DBStorage
-                        with the list of City objects linked to the State
-    """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)

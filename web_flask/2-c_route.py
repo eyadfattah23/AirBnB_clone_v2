131ab5906173a443700744339e6238aab74bb111
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-05 03:16:39 +0300

    comments and revision

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,29 +4,45 @@ script that starts a Flask web application
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed the text variable
+                (replace underscore _ symbols with a space )
 '''
 from flask import Flask
-from markupsafe import escape
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """display “Hello HBNB!”"""
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """display “Hello HBNB!”
+
+    Returns:
+        str:  “Hello HBNB!”
+    """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """display “HBNB”"""
+    """display HBNB”
+
+    Returns:
+        str:  "HBNB”
+    """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def C_text(text):
-    """display “C ” followed by the value of the text variable """
-    return f"C {escape(text.replace('_', ' '))}"
+    """display display “C ” followed by value of text variable
+
+    Returns:
+        str:  "c <text>"
+    """
+
+    return f"c {text.replace('_', ' ')}"
+    # return f"C {escape(text.replace('_', ' '))}"
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # , debug=True)

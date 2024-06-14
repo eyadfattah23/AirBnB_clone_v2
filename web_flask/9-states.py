98ea4cd1ff74db768d890d06a34acddf932b514c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-14 07:19:37 +0300

    use an easier approach

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,60 +11,36 @@
     Call in this method storage.close()
 
 - Routes:
-    /states: display a HTML page
-    /states/<id>: display a HTML page
+    /states_list: display a HTML page
 """
 from models import storage
-from models.state import State
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """display “Hello HBNB!”"""
-    return "Hello HBNB!"
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """After each request remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def list_states(id=None):
-    """display a HTML page
+def states(id=None):
+    '''display a HTML page in  9-states.html'''
+    from models.state import State
 
-    Returns:
-        html file: the list of all State objects present in DBStorage
-    """
     states = storage.all(State)
     stated = None
     if id:
         for state in states.values():
-            if state.id == id:
+            if id == state.id:
                 stated = state
                 break
 
-    return render_template('9-states.html', states=states,
-                           stated=stated, id=id)
+    return render_template('9-states.html', states=states, stated=stated)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def list_cities_by_state():
-    """display a HTML page
-
-    Returns:
-        html file: the list of all State objects present in DBStorage
-                        with the list of City objects linked to the State
-    """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    ''' remove the current SQLAlchemy Session '''
+    storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # , debug=True)

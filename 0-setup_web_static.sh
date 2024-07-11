240d6851eca92456cb0f37c61aaecf59f817c324
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-07-11 03:15:03 +0300

    add more comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,4 +23,5 @@ sudo chown -R ubuntu:ubuntu /data/
 # 7. Update the Nginx configuration to serve the content
 sudo sed -i '59i\\tlocation \/hbnb_static { \n\t\talias \/data\/web_static\/current/;\n\t}' /etc/nginx/sites-available/default
 
+#8. Restart the Nginx server
 sudo service nginx restart;

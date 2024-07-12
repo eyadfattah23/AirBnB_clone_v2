032b30bebbf2e708dae552dacb3f9c580bcbddcb
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-07-12 23:21:34 +0300

    revert

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,79 +1,55 @@
 #!/usr/bin/python3
-'''Fabric script (based on the file 1-pack_web_static.py)
-distributes an archive to your web servers,
-using the function do_deploy'''
-
-from fabric.api import run, put, env
+'''
+a Fabric script that distributes an archive to web servers
+'''
+from fabric.api import put, sudo, env
 from datetime import datetime
 import os
 
+env.hosts = ['52.91.152.150', '34.227.91.173']
+
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers
+    """Returns True if all operations have been done correctly,
+    otherwise returns False
+        Steps:
+            1- Upload the archive to the /tmp/ directory of the web server
+            2- extract the archive to /data/web_static/releases/<filename>
+            3- Delete the archive from the web server
+            4- Delete the symlink /data/web_static/current from the server
 
     Args:
-        archive_path (string): the compressed file path that will be sent
-                to the web servers
-
-    The function makes the following steps:
-
-    -Upload the archive to the /tmp/ directory of the web server
-
-    -Unzip the archive to the folder /data/web_static/releases/<archive name
-        without .tgz> on the web server
-
-    -Delete the archive from the web server
-
-    -Delete the symbolic link /data/web_static/current from the web server
-
-    -Create a new the symbolic link /data/web_static/current on the web server,
-    linked to the new version of my code
-    at (/data/web_static/releases/<archive name without .tgz>)
-
+            archive_path (str): path of the tgz to upload
     """
     try:
-        env.hosts = ['52.3.251.8', '100.25.162.125']
-        env.user = 'ubuntu'
-        # Return False if the file at the path archive_path doesn’t exist
         if not os.path.exists(archive_path):
             return False
 
-        for host in env.hosts:
-            env.host_string = host
-            # Upload the archive to the /tmp/ directory of the web server
-            put(archive_path, '/tmp/')
+        put(archive_path, '/tmp/')
+        archive_name = archive_path.split('/')[-1]
+        archive_folder = archive_name.split('.')[0]
 
-            # mkdir if non-existent
-            folder_name = archive_path.split("/")[-1][:-4]
-            archive_name = archive_path.split("/")[-1]
-            run("sudo mkdir -p /data/web_static/releases/{}"
-                .format(folder_name))
+        sudo(f'mkdir -p /data/web_static/releases/{archive_folder}')
 
-            # Uncompress the archive
-            run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-                .format(archive_name,
-                        folder_name))
+        sudo(
+            f'tar -xzf /tmp/{archive_name} -C \
+/data/web_static/releases/{archive_folder}')
 
-            # Delete the archive from the web server
-            run('rm /tmp/{}'.format(archive_name))
+        sudo(f'rm /tmp/{archive_name}')
 
-            # Delete the sym link /data/web_static/current from the web server
-            run('sudo mv /data/web_static/releases/{}/web_static/* \
-                /data/web_static/releases/{}/'
-                .format(folder_name, folder_name))
+        sudo(
+            f'mv /data/web_static/releases/{archive_folder}/web_static/* \
+/data/web_static/releases/{archive_folder}/')
 
-            run('sudo rm -rf /data/web_static/releases/{}/web_static'
-                .format(folder_name))
+        sudo(f'rm -rf /data/web_static/releases/{archive_folder}/web_static')
 
-            run('sudo rm -rf /data/web_static/current')
+        sudo('rm -rf /data/web_static/current')
 
-            # Create a new the symlink /data/web_static/current on web server,
-            # linked to the new version of your code
-            run('sudo \
-                ln -s /data/web_static/releases/{}/ /data/web_static/current'
-                .format(folder_name))
+        sudo(
+            f'ln -s /data/web_static/releases/{archive_folder}/ \
+/data/web_static/current')
 
-            print('New version deployed!')
+        print('New version deployed!')
         return True
     except Exception as e:
         return False
